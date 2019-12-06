e71034f88382468d8c835715d15719478101226b
Grant Shangreaux <shshoshin@protonmail.com>
2019-12-05 21:24:46 -0600

    Add submit button

--- index.lisp
+++ index.lisp
@@ -13,7 +13,14 @@
        :action "my-form" :method "post"
        (:div
         (:label :for "name")
-        (:input :type "text" :id "name" :name "user_name")))))))
+        (:input :type "text" :id "name" :name "name"))
+       (:div
+        (:label :for "salary")
+        (:input :type "number" :id "salary" :name "salary"))
+       (:div
+        (:label :for "age")
+        (:input :type "number" :id "age" :name "age"))
+       (:button "submit"))))))
 
 (with-open-file (f "dist/index.html" :direction :output
                                 :if-exists :supersede
