f0fc65e6045e2fffaea059cc834cacf5d1263e0f
Grant Shangreaux <shshoshin@protonmail.com>
2019-12-05 18:37:58 -0600

    Add: initial html test

---
+++ index.lisp
@@ -0,0 +1,22 @@
+(in-package :spinneret)
+
+(defun home ()
+  (with-html
+    (:doctype)
+    (:html
+     (:head
+      (:title "Home page"))
+     (:body
+      (:header
+       (:h1 "Home page"))
+      (:form
+       :action "#" :method "post"
+       (:div
+        (:label :for "name")
+        (:input :type "text" :id "name" :name "user_name")))))))
+
+(with-open-file (f "index.html" :direction :output
+                                :if-exists :supersede
+                                :if-does-not-exist :create)
+    (write-sequence (with-html-string (home)) f))
+
